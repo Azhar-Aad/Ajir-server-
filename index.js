fa152f568dbf8754dea9f2f13aae280726a365d8
Azhar-Aad <azharoo1244@gmail.com>
2025-12-15 15:42:08 +0400

    update admin

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ app.delete("/admin/delete-product/:id", async (req, res) => {
       return res.status(404).json({ message: "Product not found" });
     }
 
-    // res.json({ message: "Product deleted successfully" });
+    res.json({ message: "Product deleted successfully" });
   } catch (err) {
     res.status(500).json({ message: "Delete failed" });
   }
